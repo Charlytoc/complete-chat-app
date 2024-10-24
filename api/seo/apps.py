19bd0f346b535a03a98f868185a846ad0981fd3a
Rafael Arias <ariasnavarrorafael@gmail.com>
2024-10-24 08:59:49 -0500

    signals

--- api/seo/apps.py
+++ api/seo/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SeoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api.seo'
+    
+    def ready(self):
+        import api.seo.signals
